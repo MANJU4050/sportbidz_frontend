8c0047fd0073e7c592920deb6c07a3c59dc8c801
Manjunath <makzink40@gmail.com>
2024-02-07 16:48:49 +0530

    auth context and provider

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,16 @@ import { Provider } from 'react-redux'
 
 import App from './App.tsx'
 import { store } from './app/store.ts'
+import { AuthProvider } from './context/authcontext/index.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <ChakraProvider>
-        <App />
-      </ChakraProvider>
-    </Provider>
+    <AuthProvider>
+      <Provider store={store}>
+        <ChakraProvider>
+          <App />
+        </ChakraProvider>
+      </Provider>
+    </AuthProvider>
   </React.StrictMode>,
 )
